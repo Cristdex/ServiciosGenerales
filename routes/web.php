ba301fda4ae2ba3a754ef9c474920797289f3ec8
Cristopher <cristophores@gmail.com>
2020-05-15 03:44:54 -0500

    «ServiciosGenerales»

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
+
+Route::resource('usuarios', 'UserController')->middleware('auth');
+
+
